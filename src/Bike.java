bee4a4627baaf9c2a893ab4f3da3798acdb0b67a
SimonaR07 <sristomanova32@gmail.com>
2024-01-19 21:23:12 +0100

    Add new java program

---
+++ src/Bike.java
@@ -0,0 +1,18 @@
+
+abstract class Bike {
+	Bike (){
+		System.out.println("Bike is created. ");
+	}
+	
+	abstract void run();
+	void changeGear() {
+		System.out.println("Gear changed.");
+	}
+
+}
+
+class Honda extends Bike {
+	void run() {
+		System.out.println("Running safely...");
+	}
+}
